c1a985d64aa6332ac8a9c7205545cea40d8d7557
Vinicius Avellar Moreira <amvinicius@gmail.com>
2017-06-13 09:40:21 -0300

    Maven cache changes

--- .travis/maven_cfg.sh
+++ .travis/maven_cfg.sh
@@ -2,25 +2,28 @@
 set -ev
 cd $(dirname $(readlink -f $0))
 
-if [ -f $MAVEN_CFG/repository ]; then
-    echo "Using cached maven dependencies..."
-    exit 0
-fi
-
-if [ "$ORACLE_OTN_USER" == "" ] || [ "$ORACLE_OTN_PASSWORD" == "" ]; then
-    echo "Oracle OTN username/password not specified."
-    exit 1
-fi
-
 # Download wagon-http recommended by Oracle.
 # On maven latest version this is not needed, but travis doesn't have it.
-curl -L -O "http://central.maven.org/maven2/org/apache/maven/wagon/wagon-http/2.8/wagon-http-2.8-shaded.jar"
-sudo mv wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
+if [ ! -f $CACHE_DIR/wagon-http-2.8-shaded.jar ]; then
+    curl -L -O "http://central.maven.org/maven2/org/apache/maven/wagon/wagon-http/2.8/wagon-http-2.8-shaded.jar"
+    mv wagon-http-2.8-shaded.jar $CACHE_DIR/
+    sudo cp $CACHE_DIR/wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
+else
+    echo "Using cached wagon-http..."
+    sudo cp $CACHE_DIR/wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
+fi
 
 # Create the settings file with oracle server config.
-cp settings.xml $MAVEN_CFG/settings.xml
+# If file already exists, Oracle dependencies were cached on previous build.
+if [ ! -f $MAVEN_CFG/.cached ]; then
+    cp settings.xml $MAVEN_CFG/settings.xml
+    touch $MAVEN_CFG/.cached
+else
+    echo "Using cached maven settings..."
+fi
 
 # The Java API is not available on a public repository yet, we need to download and install it locally.
+# Always downloading the latest development version.
 git clone https://github.com/utPLSQL/utPLSQL-java-api.git
 cd utPLSQL-java-api
 mvn package install -DskipTests
