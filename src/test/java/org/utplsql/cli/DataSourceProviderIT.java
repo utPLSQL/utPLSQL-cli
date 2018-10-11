8d832617451cc70924bbeec10857a744a2903d9d
pesse <derpesse@gmail.com>
2018-10-11 09:14:31 +0200

    Add ConnectionInit-SQL based on NLS_LANG
    Remove NLS_LANG from Locale-Initialization
    Fixes #101

--- src/test/java/org/utplsql/cli/DataSourceProviderIT.java
+++ src/test/java/org/utplsql/cli/DataSourceProviderIT.java
@@ -12,55 +12,67 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
-public class DataSourceProviderIT {
+class DataSourceProviderIT {
 
     @Test
-    public void connectToDatabase() throws IOException, SQLException {
-
-        ConnectionConfig config = new ConnectionConfig(TestHelper.getConnectionString());
-
-        DataSource dataSource = new TestedDataSourceProvider(config).getDataSource();
+    void connectToDatabase() throws SQLException {
+        DataSource dataSource = getDataSource();
 
         assertNotNull(dataSource);
     }
 
     @Test
-    public void initNlsLang() throws SQLException {
-        ConnectionConfig config = new ConnectionConfig(TestHelper.getConnectionString());
+    void initNlsLang() throws SQLException {
         System.setProperty("NLS_LANG", "BRAZILIAN PORTUGUESE_BRAZIL.WE8ISO8859P1");
-
-
-        DataSource dataSource = new TestedDataSourceProvider(config).getDataSource();
+        DataSource dataSource = getDataSource();
 
         assertNotNull(dataSource);
-
-        try ( Connection con = dataSource.getConnection() ) {
-            try (PreparedStatement stmt = con.prepareStatement("select value from nls_session_parameters where parameter = 'NLS_LANGUAGE'")) {
-                ResultSet rs = stmt.executeQuery();
-                if ( rs.next() ) {
-                    assertEquals("BRAZILIAN PORTUGUESE", rs.getString(1));
-                }
-            }
-        }
+        checkNlsSessionParameter(dataSource, "NLS_LANGUAGE", "BRAZILIAN PORTUGUESE");
+        checkNlsSessionParameter(dataSource, "NLS_TERRITORY", "BRAZIL");
     }
 
     @Test
-    public void initPartialNlsLang() throws SQLException {
-        ConnectionConfig config = new ConnectionConfig(TestHelper.getConnectionString());
+    void initPartialNlsLangTerritory() throws SQLException {
         System.setProperty("NLS_LANG", "_SOMALIA");
-
-
-        DataSource dataSource = new TestedDataSourceProvider(config).getDataSource();
+        DataSource dataSource = getDataSource();
 
         assertNotNull(dataSource);
+        checkNlsSessionParameter(dataSource, "NLS_TERRITORY", "SOMALIA");
+    }
 
+    @Test
+    void initPartialNlsLangLanguage() throws SQLException {
+        System.setProperty("NLS_LANG", "HINDI");
+        DataSource dataSource = getDataSource();
+
+        assertNotNull(dataSource);
+        checkNlsSessionParameter(dataSource, "NLS_LANGUAGE", "HINDI");
+    }
+
+    @Test
+    void initNlsLangEmpty() throws SQLException {
+        System.setProperty("NLS_LANG", "");
+        DataSource dataSource = getDataSource();
+
+        assertNotNull(dataSource);
+    }
+
+    private DataSource getDataSource() throws SQLException {
+        ConnectionConfig config = new ConnectionConfig(TestHelper.getConnectionString());
+        return new TestedDataSourceProvider(config).getDataSource();
+    }
+
+    private void checkNlsSessionParameter( DataSource dataSource, String parameterName, String expectedValue ) throws SQLException {
         try ( Connection con = dataSource.getConnection() ) {
-            try (PreparedStatement stmt = con.prepareStatement("select value from nls_session_parameters where parameter = 'NLS_TERRITORY'")) {
+            try (PreparedStatement stmt = con.prepareStatement("select value from nls_session_parameters where parameter = ?")) {
+                stmt.setString(1, parameterName);
                 ResultSet rs = stmt.executeQuery();
-                if ( rs.next() ) {
-                    assertEquals("SOMALIA", rs.getString(1));
-                }
+                if ( rs.next() )
+                    assertEquals(expectedValue, rs.getString(1));
+                else
+                    fail("Could not get NLS Session parameter value for '" + parameterName + "'");
             }
         }
     }
