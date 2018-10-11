3513f49df275866ecf71be8defd581707cf67dc0
pesse <derpesse@gmail.com>
2018-10-11 08:32:35 +0200

    Test to parse/set NLS_LANG correctly

--- src/test/java/org/utplsql/cli/DataSourceProviderIT.java
+++ src/test/java/org/utplsql/cli/DataSourceProviderIT.java
@@ -5,8 +5,12 @@ import org.utplsql.cli.datasource.TestedDataSourceProvider;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DataSourceProviderIT {
@@ -19,5 +23,45 @@ public class DataSourceProviderIT {
         DataSource dataSource = new TestedDataSourceProvider(config).getDataSource();
 
         assertNotNull(dataSource);
+    }
+
+    @Test
+    public void initNlsLang() throws SQLException {
+        ConnectionConfig config = new ConnectionConfig(TestHelper.getConnectionString());
+        System.setProperty("NLS_LANG", "BRAZILIAN PORTUGUESE_BRAZIL.WE8ISO8859P1");
+
+
+        DataSource dataSource = new TestedDataSourceProvider(config).getDataSource();
+
+        assertNotNull(dataSource);
+
+        try ( Connection con = dataSource.getConnection() ) {
+            try (PreparedStatement stmt = con.prepareStatement("select value from nls_session_parameters where parameter = 'NLS_LANGUAGE'")) {
+                ResultSet rs = stmt.executeQuery();
+                if ( rs.next() ) {
+                    assertEquals("BRAZILIAN PORTUGUESE", rs.getString(1));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void initPartialNlsLang() throws SQLException {
+        ConnectionConfig config = new ConnectionConfig(TestHelper.getConnectionString());
+        System.setProperty("NLS_LANG", "_SOMALIA");
+
+
+        DataSource dataSource = new TestedDataSourceProvider(config).getDataSource();
+
+        assertNotNull(dataSource);
+
+        try ( Connection con = dataSource.getConnection() ) {
+            try (PreparedStatement stmt = con.prepareStatement("select value from nls_session_parameters where parameter = 'NLS_TERRITORY'")) {
+                ResultSet rs = stmt.executeQuery();
+                if ( rs.next() ) {
+                    assertEquals("SOMALIA", rs.getString(1));
+                }
+            }
+        }
     }
 }
