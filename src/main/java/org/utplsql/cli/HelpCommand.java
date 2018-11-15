cc816dced4d1aee5decf99b720a8139d2ffcb00f
pesse <derpesse@gmail.com>
2018-11-15 23:07:12 +0100

    Make sure cli outputs usage in case of an error
    For example when no command is provided.
    Fixes #110

--- src/main/java/org/utplsql/cli/HelpCommand.java
+++ src/main/java/org/utplsql/cli/HelpCommand.java
@@ -1,19 +1,38 @@
 package org.utplsql.cli;
 
+import com.beust.jcommander.JCommander;
+
+/** Simple Help-Command which outputs an (optional) error message and the command line usage
+ * @author pesse
+ */
 public class HelpCommand implements ICommand {
 
     private String errorMessage;
+    private JCommander jCommander;
 
-    public HelpCommand( String errorMessage ) {
+    public HelpCommand(JCommander jCommander) {
+        this.jCommander = jCommander;
+    }
+
+    public HelpCommand( JCommander jCommander, String errorMessage ) {
+        this.jCommander = jCommander;
         this.errorMessage = errorMessage;
     }
 
     @Override
     public int run() {
-        if ( errorMessage != null )
+        if ( errorMessage != null ) {
             System.out.println(errorMessage);
-
-        return 1;
+            if (jCommander.getParsedCommand() != null)
+                jCommander.usage(jCommander.getParsedCommand());
+            else
+                jCommander.usage();
+            return 1;
+        }
+        else {
+            jCommander.usage();
+            return 0;
+        }
     }
 
     @Override
