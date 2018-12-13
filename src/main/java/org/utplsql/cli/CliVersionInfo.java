3c0c467177bf44d28b469c3411ccf56cffe4d50c
pesse <derpesse@gmail.com>
2018-12-14 00:06:37 +0100

    Put version info into utplsql-cli.version file and read it
    instead of compiling it into code.

--- src/main/java/org/utplsql/cli/CliVersionInfo.java
+++ src/main/java/org/utplsql/cli/CliVersionInfo.java
@@ -1,5 +1,13 @@
 package org.utplsql.cli;
 
+import org.utplsql.api.JavaApiVersionInfo;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /** This class is getting updated automatically by the build process.
  * Please do not update its constants manually cause they will be overwritten.
  *
@@ -7,14 +15,23 @@ package org.utplsql.cli;
  */
 public class CliVersionInfo {
 
-    private static final String BUILD_NO = "local";
-    private static final String MAVEN_PROJECT_NAME = "cli";
-    private static final String MAVEN_PROJECT_VERSION = "3.1.1-SNAPSHOT";
+    private static final String MAVEN_PROJECT_NAME = "utPLSL-cli";
+    private static String MAVEN_PROJECT_VERSION = "unknown";
 
-    public static String getVersion() {
-        return MAVEN_PROJECT_VERSION + "." + BUILD_NO;
+    static {
+        try {
+            MAVEN_PROJECT_VERSION = Files.readAllLines(
+                    Paths.get(CliVersionInfo.class.getClassLoader().getResource("utplsql-cli.version").toURI())
+                    , Charset.defaultCharset())
+                    .get(0);
+        }
+        catch ( IOException | URISyntaxException e ) {
+            System.out.println("WARNING: Could not get Version information!");
+        }
     }
 
+    public static String getVersion() { return MAVEN_PROJECT_VERSION; }
     public static String getInfo() { return MAVEN_PROJECT_NAME + " " + getVersion(); }
+
 
 }
