6922e5256d4b03d55bcbca09c3b64343017e50b2
pesse <derpesse@gmail.com>
2019-06-13 16:57:50 +0200

    Removed some now unnecessary stuff

--- src/main/java/org/utplsql/cli/Cli.java
+++ src/main/java/org/utplsql/cli/Cli.java
@@ -1,7 +1,5 @@
 package org.utplsql.cli;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.ParameterException;
 import picocli.CommandLine;
 
 import java.util.List;
@@ -10,8 +8,6 @@ public class Cli {
 
     static final int DEFAULT_ERROR_CODE = 1;
 
-    static final String HELP_CMD = "-h";
-
     public static void main(String[] args) {
 
         int exitCode = runPicocliWithExitCode(args);
@@ -19,40 +15,6 @@ public class Cli {
         System.exit(exitCode);
     }
 
-    static int runWithExitCode( String[] args ) {
-
-        LoggerConfiguration.configure(LoggerConfiguration.ConfigLevel.NONE);
-        LocaleInitializer.initLocale();
-
-        JCommander jc = new JCommander();
-        jc.setProgramName("utplsql");
-
-        CommandProvider cmdProvider = new CommandProvider(jc);
-
-        cmdProvider.commands().forEach(cmd -> jc.addCommand(cmd.getCommand(), cmd));
-
-        int exitCode = DEFAULT_ERROR_CODE;
-
-        if ( args.length >= 1 && args[0].equals("-h") ) // Help?
-        {
-            exitCode = 0;
-            jc.usage();
-        }
-        else {
-            try {
-                jc.parse(args);
-
-                exitCode = cmdProvider.getCommand(jc.getParsedCommand()).run();
-
-            } catch (ParameterException e) {
-                exitCode = new HelpCommand(jc, e.getMessage()).run();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        return exitCode;
-    }
     static int runPicocliWithExitCode( String[] args ) {
 
         LoggerConfiguration.configure(LoggerConfiguration.ConfigLevel.NONE);
