805595ea2d8de70dd255c50ae7c35d0c2896840f
Jacek Gębal <jacek.gebal@smenso.de>
2026-04-14 14:57:20 +0300

    Attempting to fix Java11+ compatibility issue.

--- src/main/java/org/utplsql/cli/datasource/InitializableOracleDataSource.java
+++ src/main/java/org/utplsql/cli/datasource/InitializableOracleDataSource.java
@@ -6,7 +6,6 @@ import javax.sql.DataSource;
 import java.io.PrintWriter;
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.ConnectionBuilder;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
 import java.util.logging.Logger;
