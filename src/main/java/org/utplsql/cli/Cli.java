73ee0a8a20adeacf431fad6e3787f5ee68f25823
Jacek Gębal <jacek.gebal@smenso.de>
2026-07-10 13:48:02 +0300

    Fixing issue with logger inheriting DEBUG mode after previous test run. Alternative implementation.

--- src/main/java/org/utplsql/cli/Cli.java
+++ src/main/java/org/utplsql/cli/Cli.java
@@ -21,7 +21,6 @@ public class Cli {
 
     static int runPicocliWithExitCode(String[] args) {
 
-        LoggerConfiguration.configure(LoggerConfiguration.ConfigLevel.NONE);
 
         logger.debug("Args: "+String.join(", ", args));
 
